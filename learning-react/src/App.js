0c636706351355a97770d9ea6ab7915a10d973c3
Pradeep Tukaram Kadam <pradeeptraje@gmail.com>
2022-11-22 11:31:20 +0530

    React JS Started with Welcome

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import App from "./Appx";
+import LoginPage from "./component/login";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Welcome to react </h1>
+      <LoginPage></LoginPage>
     </div>
   );
-}
+};
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello, Welcome to Home Page</h1>
+//     </div>
+//   );
+// }
 
 export default App;
