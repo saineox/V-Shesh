03b205dc2564065e8123b8ae2309d12a84059290
Pradeep Tukaram Kadam <pradeeptraje@gmail.com>
2022-11-24 19:00:54 +0530

    React js css update for images

--- learning-react/src/components/login.js
+++ learning-react/src/components/login.js
@@ -3,8 +3,10 @@ import React from "react";
 const LoginPage = () => {
   return(
     <div className="login">
-
-      <img src={require("../images/jz.jpg")} width="200" alt="Janat" />
+      <div className="loginitem">
+      <img src={require("../images/jz.jpg")} height="250vh" alt="Janat" />
+      <img src="https://filmfare.wwmindia.com/content/2021/aug/youngest-bollywood-actress-sanjana-sanghi-51629879242.jpg" height="250vh" alt="Sushant"/>
+      </div>
     <div className="space">
       <label className="align-label">Enter your Email ID</label>
       <input type="text" className="align-input" placeholder="Please enter email id" />
